e9c849f9a08e5b711d2502ee53ba2d7e331b177e
whitelilis <liu.zhe@admaster.com.cn>
2017-09-26 13:31:40 +0800

    remove target; add toDF support

--- src/main/scala/drunkedcat/BatchJob.scala
+++ src/main/scala/drunkedcat/BatchJob.scala
@@ -6,12 +6,17 @@ import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.hadoop.io.{LongWritable, Text}
 import org.apache.hadoop.mapreduce.lib.input.{CombineTextInputFormat, TextInputFormat}
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
-
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types._
 
 
 
 object BatchJob {
+
+
+
+
+
   def main(args: Array[String]) {
     val spark = SparkSession
       .builder
@@ -19,38 +24,40 @@ object BatchJob {
       //.master("local")
       .getOrCreate()
     SparkSession.builder().config(new SparkConf)
+    import spark.implicits._
 
     val input = args(0)
     val output = args(1)
 
     val fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
     val path = new Path(output)
-    if(fs.exists(path)){
+    if (fs.exists(path)) {
       fs.delete(path, true)
     }
 
-    
+
     // when using as  :  val partitionCount = if xxxxx else xxxx,    repartition()  will say error, why ?
     var partitionCount = 400
-    if(args.length > 2){
+    if (args.length > 2) {
       partitionCount = Integer.decode(args(2))
-    }else{
+    } else {
       //skip
     }
-    /*
+    /*  rdd
     spark.sparkContext.hadoopConfiguration.setInt("mapreduce.input.fileinputformat.split.maxsize", 256 * 1024 * 1024)
     val files = spark.sparkContext.newAPIHadoopFile(input, classOf[CombineTextInputFormat], classOf[LongWritable], classOf[Text], spark.sparkContext.hadoopConfiguration)
-    val count = files.map{
-      case (offset, line) => {
-        line.toString().split(",", -1)(77)
+    val rdd = files.map {
+      case (_, line) => {
+        TrackRecord.row(line.toString().split(",", -1).slice(0, 2))
       }
-    }.distinct()
-      .saveAsTextFile("/tmp/packaged")
+    }
     */
 
+    /* sql
+    spark.sqlContext.read.csv(input).createOrReplaceTempView("track")
+    spark.sql(query).write.csv(output)
+    */
 
-    spark.read.json(input).repartition(partitionCount).registerTempTable("re_tmp")
-    spark.sql("select m6,m7,m6a,count(*) from re_tmp where (m6 is not null) or (m7 is not null) or (m6a is not null) group by m6,m7,m6a").write.csv(output)
     spark.stop()
   }
 }
